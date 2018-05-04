3d60c71f952c08135989f1b897efb7b337326f4a
Developer <developer@local>
2018-05-03 20:11:39 -0700

    added template for settings

---
+++ template_secret_info.py
@@ -0,0 +1,7 @@
+
+# to add bot to a server:
+# https://discordapp.com/oauth2/authorize?client_id=XXXXXXXXXXXXXXXXXX&scope=bot
+
+CLIENT_ID = "XXXXXXXXXXXXXXXXXX"
+TOKEN = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
