994d80dba21f54374bb1577b04ae366395b072b5
0x1d00ffff <x1d@protonmail.com>
2020-11-14 14:24:56 -0800

    halve the price update rate until I can implement some infura efficiency improvements

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ from mock_discord_classes import MockClient, MockMessage, MockAuthor
 
 
 _PROGRAM_NAME = "0xbtc-discord-price-bot"
-_VERSION = "0.4.8"
+_VERSION = "0.4.9"
 
 
 old_status_string = None
