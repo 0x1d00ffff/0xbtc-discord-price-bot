41616ad8f3340e13e7f199f345b808957d5c22d9
0x1d00ffff <x1d@protonmail.com>
2020-11-14 15:03:47 -0800

    add an expected error to main() exception handler to make exiting the bot easier with ctrl+c

--- main.py
+++ main.py
@@ -562,11 +562,20 @@ def main():
                 background_update.start()
                 client.run(TOKEN)
             except (SystemExit, KeyboardInterrupt):
-                raise
-            except:
+                # what we expect to get when exiting with Ctrl+C
+                break
+            except RuntimeError as e:
+                if str(e) == "Event loop is closed":
+                    # this error is sometimes thrown when exiting the bot with Ctrl+C
+                    break
+                else:
+                    logging.exception('Unexpected error from Discord... retrying')
+                    time.sleep(10)  # wait a little time to prevent cpu spins
+            except Exception:
                 logging.exception('Unexpected error from Discord... retrying')
                 time.sleep(10)  # wait a little time to prevent cpu spins
 
+        logging.info("Exiting...bye!")
 
 if __name__ == "__main__":
     main()
