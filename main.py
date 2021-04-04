6370d900aabc7c6efd22feee316e9daecbf992e4
0x1d00ffff <x1d@protonmail.com>
2021-04-04 11:29:43 -0700

    bump to 0.5.1

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ from mock_discord_classes import MockClient, MockMessage, MockAuthor
 
 
 _PROGRAM_NAME = "0xbtc-discord-price-bot"
-_VERSION = "0.5.0"
+_VERSION = "0.5.1"
 
 
 old_status_string = None
